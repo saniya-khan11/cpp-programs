09be8b1d1f207d32d2f06f569e03e4fd8021b19c
saniya-khan11 <146875076+saniya-khan11@users.noreply.github.com>
2024-12-07 03:31:26 +0530

    changing the value at index 2 of marks array using pointer

--- pointers-arithmetic-by-code-with-harry.cpp
+++ pointers-arithmetic-by-code-with-harry.cpp
@@ -12,6 +12,8 @@ int main()
     cout<<"Value at marks [2] is : "<<*(p+2)<<endl;
     cout<<"Value at marks [3] is : "<<*(p+3)<<endl;
     cout<<endl<<"Printing values using for loop :-"<<endl;
+    //changing one of the value of array using pointer
+    p[2]=5;
     for (int i=0;i<4;i++)
     {
         cout<<"Value at marks["<<i<<"] is : "<<*(p+i)<<endl;
