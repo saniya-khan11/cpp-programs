d856e0e1c40ee144c6bf6adbe01b668c469a4a57
saniya-khan11 <146875076+saniya-khan11@users.noreply.github.com>
2024-12-07 14:39:17 +0530

    Update class-by-freecodecamp.cpp

--- class-by-freecodecamp.cpp
+++ class-by-freecodecamp.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 class Employee{
-    public:
+    public:        /*public is access modifier.
+                    All the class members declared 
+                    under the public specifier will be available to everyone.*/
         string Name;
         string Company;
         int Age;
