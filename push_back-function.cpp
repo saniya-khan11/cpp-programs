733e43a4c6980b8a8d14d0e0780a9e5503be06d9
saniya-khan11 <146875076+saniya-khan11@users.noreply.github.com>
2024-12-08 22:43:56 +0530

    Create push_back-function.cpp

---
+++ push_back-function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str = "Hello";
+    str.push_back('!'); // Appends the character '!'
+    cout << str << endl; // Output: Hello!
+    return 0;
+}
